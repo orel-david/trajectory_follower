e3d3a3e355a99c79216f4048c1ca00d887fd4426
oreld <orel.david1502@gmail.com>
2019-10-27 18:52:02 +0200

    Add the robot width constant

--- src/main/java/robot/Constants.java
+++ src/main/java/robot/Constants.java
@@ -16,6 +16,7 @@ public class Constants {
         public static final double TIME_STEP = 0.02;
         public static final double MAX_ACCELERATION = 0.4;// in m/s^2 (currently not the correct number)
         public static final int MAX_CURRENT = 35;
+        public static final double DRIVETRAIN_WIDTH = 0.75;
     }
 
 
