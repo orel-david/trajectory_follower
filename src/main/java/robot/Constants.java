d9b27e19a04209e374f8ff4a66ee4eb880004b76
Unknown <orel.david1502@gmail.com>
2019-11-09 22:02:41 +0200

    Fix the ticks per meter constant

--- src/main/java/robot/Constants.java
+++ src/main/java/robot/Constants.java
@@ -11,7 +11,7 @@ public class Constants {
 
 
     public static class Drivetrain {
-        public static final double TICKS_PER_METER = 256 / (4 * 0.0254 * Math.PI);
+        public static final double TICKS_PER_METER = 1920;
         public static final double MAX_VEL = 3;// in m/s
         public static final double TIME_STEP = 0.02;
         public static final double MAX_ACCELERATION = 0.4;// in m/s^2 (currently not the correct number)
